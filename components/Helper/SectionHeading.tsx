87121cd9d6318360bb0ec9e51b640872a04af6d2
sankarkarmakar01 <karmakarsankar000@gmail.com>
2025-04-05 20:28:13 +0530

    Commit 05-04-2025

--- components/Helper/SectionHeading.tsx
+++ components/Helper/SectionHeading.tsx
@@ -7,10 +7,12 @@ type Props = {
 
 const SectionHeading = ({ children }: Props) => {
   return (
-    <h1 className="bg-blue-800 w-fit text-center px-4 py-3 mx-auto text-white text-2xl sm:text-3xl md:text-4xl uppercase -rotate-6">
+    <h1 className="bg-blue-600 w-fit text-center px-4 py-3 mx-auto text-white text-2xl sm:text-3xl md:text-4xl uppercase rounded-4xl font-semibold">
       {children}
     </h1>
   );
 };
 
 export default SectionHeading;
+
+// -rotate-6
--- components/Home/Navbar/MobileNav.tsx
+++ components/Home/Navbar/MobileNav.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { navLinks } from "../../../constant/constant";
 import Link from "next/link";
 import React from "react";
